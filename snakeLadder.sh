b8bed9084537d13c574fb98dc19a9b6f6dacde66
vkshay3400 <akshayp2208@gmail.com>
2020-02-24 15:45:15 +0530

    Added: Rolldie in snakeladder

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -5,6 +5,12 @@ echo "*************************Snake and Ladder************************"
 # CONSTANTS
 WIN_POSITION=100
 START_POSITION=0
+COUNT=0
 
 # VARIABLE
 position=0
+currentposition=$position
+
+# TO GET RANDOM NUMBER 
+random1=$((1+RANDOM%6))
+
