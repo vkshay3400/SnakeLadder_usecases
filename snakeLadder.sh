9410b1338cba81cbe8176aa970b67b869b946b9a
vkshay3400 <akshayp2208@gmail.com>
2020-02-24 16:14:47 +0530

    Added: Repeat till 100 and restart from zero

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -5,7 +5,15 @@ echo "*************************Snake and Ladder************************"
 # FUNCTION FOR SNAKE
 function snake(){
 	position=$1
-	position=$(($1-$random1))	
+	position=$(($1-$random1))		
+	if (( $1 < $START_POSITION ))
+	then
+		position=$START_POSITION
+	fi
+	if (( $position < $WIN_POSITION ))
+	then
+		currentposition=$1
+	fi
 }
 
 # FUNCTION FOR LADDER
@@ -16,17 +24,23 @@ function ladder(){
 
 # CASE STATEMENTS FOR CHECKING CONDITION
 function players(){
+# CONSTANTS
+WIN_POSITION=100
+START_POSITION=0
 
 # VARIABLE
 position=0
 currentposition=$position
+
+while [[ $position -ne $WIN_POSITION ]]
+do
 random1=$((1+RANDOM%6))
 random2=$((1+RANDOM%3))
 
 case $random2 in
 		1)
-		position=$position
-		echo SamePosition : $position
+			position=$position
+			echo SamePosition : $position
 		;;
 		2) 
 			ladder $position
@@ -36,7 +50,9 @@ case $random2 in
 			snake $position
 			echo Snake -$random1 : $position
 		;;
+		
 esac
+done
 }
 
 # TO PRINT PLAYERS
