2c96db4136328603023103261e495dba8ef45de3
vkshay3400 <akshayp2208@gmail.com>
2020-02-28 10:26:30 +0530

    Added: To check who wins the game

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -12,27 +12,30 @@ START_POSITION=0
 
 # VARIABLE
 position=0
-currentposition=$position
+currentposition=0
+flag=0
+player1=0
+player2=0
 
 # FUNCTION FOR SNAKE
 function snake(){
 	position=$1
-	position=$(($1-$randomDice))
+	position=$(($position-$randomDice))
 	if (( $1 < $START_POSITION ))
 	then
 		position=$START_POSITION
 	fi
 	if (( $position < $WIN_POSITION ))
 	then
-		currentposition=$1
+		currentposition=$position
 	fi
 }
 
 # FUNCTION FOR LADDER
 function ladder(){
 	position=$1
-	position=$(($1+$randomDice))
-	if (( $1 > $WIN_POSITION ))
+	position=$(($position+$randomDice))
+	if (( $position > $WIN_POSITION ))
 	then
 		position=$currentposition
 		currentposition=$position
@@ -41,8 +44,6 @@ function ladder(){
 
 # CASE STATEMENTS FOR CHECKING CONDITION
 function players(){
-while [[ $position -ne $WIN_POSITION ]]
-do
 # GET RANDOM FOR DICE
 randomDice=$((1+RANDOM%6))
 randomCase=$((1+RANDOM%3))
@@ -50,7 +51,7 @@ randomCase=$((1+RANDOM%3))
 case $randomCase in
 		$NO_PLAY)
 			position=$position
-			echo SamePosition : $position
+			echo "SamePosition : $position"
 		;;
 		$LADDER)
 			ladder $position
@@ -58,13 +59,45 @@ case $randomCase in
 		;;
 		*)
 			snake $position
-			echo Snake -$randomDice : $position
+			echo "Snake -$randomDice : $position"
 		;;
 esac
-(( COUNT++ ))
+}
+
+# To CHECK WIN POSITION
+function winPosition(){
+while [[ $position -ne $WIN_POSITION ]]
+do
+	playerSwitch
 done
 }
 
-# TO PRINT PLAYERS
-players
-echo "Times of dice played: $COUNT"
+# TO SWITCH PLAYER
+function playerSwitch(){
+if [ $flag -eq 0 ]
+then
+	position=$player1
+	players
+	player1=$position
+	flag=1
+else
+	position=$player2
+	players
+	player2=$position
+	flag=0
+fi
+}
+
+# TO CHECK WHO WINS FIRST
+function checkWin(){
+	if [ $player1 -eq 100 ]
+	then
+		echo "Player1 wins"
+	else
+		echo "Player2 wins"
+	fi
+}
+
+# TO CALL FUNCTION
+winPosition
+checkWin
